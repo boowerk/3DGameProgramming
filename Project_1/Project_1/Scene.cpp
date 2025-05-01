c8100efe82864888c98844157f8e061f72e11ccf
boowerk <back20044@gmail.com>
2025-05-01 15:48:37 +0900

    타이틀, 이름 렌더링

--- Project_1/Project_1/Scene.cpp
+++ Project_1/Project_1/Scene.cpp
@@ -20,19 +20,26 @@ void CScene::OnProcessingKeyboardMessage(HWND hWnd, UINT nMessageID, WPARAM wPar
 
 void CScene::BuildObjects()
 {
-	CMesh* textMesh = CMesh::LoadOBJToMesh("title.obj");
-	
+	CMesh* titleMesh = CMesh::LoadOBJToMesh("projectTitle.obj");
+	CMesh* nameMesh = CMesh::LoadOBJToMesh("name.obj");
 
 
-	m_nObjects = 1;
+	m_nObjects = 2;
 	m_ppObjects = new CGameObject * [m_nObjects];
 
 	m_ppObjects[0] = new CGameObject();
-	m_ppObjects[0]->SetMesh(textMesh);
+	m_ppObjects[0]->SetMesh(titleMesh);
 	m_ppObjects[0]->SetPosition(-4.5f, 5.0f, -30.0f);
 	m_ppObjects[0]->SetRotation(0.0f, 0.0f, 0.0f);
 	m_ppObjects[0]->SetRotationSpeed(90.0, 0.0f, 0.0f);
 	m_ppObjects[0]->SetColor(RGB(255, 255, 0));
+
+	m_ppObjects[1] = new CGameObject();
+	m_ppObjects[1]->SetMesh(nameMesh);
+	m_ppObjects[1]->SetPosition(-1.5f, 2.0f, -30.0f);
+	m_ppObjects[1]->SetRotation(0.0f, 0.0f, 0.0f);
+	m_ppObjects[1]->SetRotationSpeed(90.0, 0.0f, 0.0f);
+	m_ppObjects[1]->SetColor(RGB(255, 0, 255));
 }
 
 void CScene::ReleaseObjects()
